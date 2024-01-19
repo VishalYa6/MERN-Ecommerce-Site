d8b5d27217b431fc11e066195cac4519367467e6
Vishal Yadav <vy372984@gmail.com>
2024-01-19 14:54:01 +0530

    Initial commit

---
+++ backend/utils/token.js
@@ -0,0 +1,8 @@
+// require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const createNewToken = (payload) => {
+    return jwt.sign({ userId: payload }, process.env.SECRET_KEY, { expiresIn: '10d' });
+}
+
+module.exports = { createNewToken }
